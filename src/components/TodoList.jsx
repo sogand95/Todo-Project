be6e20aed72bee12c348d1298f303f59b6c2e9f6
unknown <sogand.aghahoseini@gmail.com>
2024-08-20 06:18:30 -0700

    first commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,30 @@
+import { useSelector } from "react-redux";
+import TodoItem from "./TodoItem";
+
+const TodoList = () => {
+  const filteredTodos = useSelector((state) => {
+    const todos = state.todos;
+    const filter = state.filter;
+    const searchTerm = state.searchTerm.toLowerCase();
+
+    return todos.filter((todo) => {
+      const matchsFilter =
+        (filter === "COMPLETED" && todo.completed) ||
+        (filter === "INCOMPLETE" && !todo.completed) ||
+        filter === "ALL";
+      const matchsSearch = todo.text.toLowerCase().includes(searchTerm);
+      return matchsFilter && matchsSearch;
+    });
+  });
+
+  return (
+    <ul>
+      <li className="my-2 text-sm italic">All Your Notes Here...</li>
+      {filteredTodos.map((todo, index) => (
+        <TodoItem key={index} todo={todo} index={index} />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
